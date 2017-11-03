fc989640d4f7467201f4f15bed865f973d393f0e
Ali Ariff <ali.ariff12@gmail.com>
2017-11-03 21:26:50 +0100

    add cors

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -2,6 +2,8 @@ class OffersController < ApplicationController
   before_action :authenticate_user!
 
   def index
-    puts current_user.to_json
+    @locations = Location.within(10, origin: current_user.location).where.not(user: current_user)
+    @articles = Offer.all()
+    render json: @articles
   end
 end
