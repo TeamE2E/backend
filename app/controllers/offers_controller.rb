f49b8bf03a5784a2f2edf4e41d4b180b28b4dc7d
Ali Ariff <ali.ariff12@gmail.com>
2017-11-03 21:14:31 +0100

    update

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,4 +1,6 @@
 class OffersController < ApplicationController
+  before_action :authenticate_user!
+
   def index
     puts current_user.to_json
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,8 @@
 class User < ActiveRecord::Base
+  has_one :location
+
   # Include default devise modules.
   devise :database_authenticatable, :registerable,
     :recoverable, :rememberable, :trackable, :validatable
   include DeviseTokenAuth::Concerns::User
-
-  has_one :location
-  acts_as_mappable through: :location
 end
