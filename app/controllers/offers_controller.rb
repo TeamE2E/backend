99b58cfc969d2867416015dcb3c8873544fccf1a
Ali Ariff <ali.ariff12@gmail.com>
2017-11-03 23:39:45 +0100

    add distance

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -8,8 +8,9 @@ class OffersController < ApplicationController
     @offers.each do |offer|
       offer.fee = offer.price_cents * 10 / 100
       offer.total_amount = offer.price_cents + offer.fee
+      offer.distance = offer.user.location.distance_from(current_user.location)
     end
 
-    render json: @offers, include: [user: {include: :location}], methods: [:fee, :total_amount]
+    render json: @offers, include: [user: {include: :location}], methods: [:fee, :total_amount, :distance]
   end
 end
--- app/models/offer.rb
+++ app/models/offer.rb
@@ -2,7 +2,7 @@ class Offer < ActiveRecord::Base
   belongs_to :user
   monetize :price_cents
 
-  attr_accessor :fee, :total_amount
+  attr_accessor :fee, :total_amount, :distance
 
   validates_presence_of :user, :price_cents, :price_currency, :available_kwh, :min_kwh
 end
