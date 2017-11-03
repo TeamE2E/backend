40f9628ed3d3bd7a140039ee4a7861089c968669
Ali Ariff <ali.ariff12@gmail.com>
2017-11-03 23:15:19 +0100

    add fee

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -5,7 +5,11 @@ class OffersController < ApplicationController
     locations = Location.within(10, origin: current_user.location).where.not(user: current_user)
     user_ids = locations.map(&:user_id)
     @offers = Offer.where(user_id: user_ids)
+    @offers.each do |offer|
+      offer.fee = offer.price_cents * 10 / 100
+      offer.total_amount = offer.price_cents + offer.fee
+    end
 
-    render json: @offers
+    render json: @offers, include: [user: {include: :location}], methods: [:fee, :total_amount]
   end
 end
