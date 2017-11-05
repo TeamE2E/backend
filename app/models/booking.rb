dd6082ae6515cae8ab4f50629a1c10b01b035e42
Ali Ariff <ali.ariff12@gmail.com>
2017-11-05 09:25:49 +0100

    money

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,6 +2,8 @@ class Booking < ActiveRecord::Base
   belongs_to :buyer, class_name: 'User', foreign_key: :buyer_id
   belongs_to :seller, class_name: 'User', foreign_key: :seller_id
 
+  monetize :price_cents, :fee_cents, :total_amount_cents
+
   validates_presence_of :buyer, :seller, :lat, :lng, :price_cents, :fee_cents, :total_amount_cents, :kwh
 
   PERCENTAGE_FEE = 10
